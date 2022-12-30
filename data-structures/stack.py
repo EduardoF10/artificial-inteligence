d8d2562071072165edc035dd847231f0e8724d69
EduardoF10 <eduardo.fuertes1@upr.edu>
2022-12-30 18:10:02 -0400

    Updated stack implementation

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -5,6 +5,12 @@ This module defines the stack data structure.
 
 class Stack(list):
 
+	def __init__(self, iterable = None):
+		if iterable == None:
+			list.__init__(self)
+		else:
+			list.__init__(self, iterable)
+
 	def push(self, item):
 		self.append(item)
 
@@ -14,14 +20,31 @@ class Stack(list):
 	def isEmpty(self):
 		return len(self) == 0
 
+	def size(self):
+		return len(self)
+
 
 # Example usage
-
 if __name__ == '__main__':
-	stack = Stack()
-	print(stack.isEmpty())
-	stack.push(7)
-	print(stack.isEmpty())
-	print(stack.pop())
-	print(stack.isEmpty())
-
+	stack1 = Stack()
+	stack2 = Stack([3, 7, 4])
+	print("Stack1:", stack1)
+	print("Stack2:", stack2)
+	print("Stack1 is empty?", stack1.isEmpty())
+	print("Stack2 is empty?", stack2.isEmpty())
+	stack1.push(3)
+	stack1.push(4)
+	stack1.pop()
+	stack1.push(7)
+	print("Stack1: [(push, 3), (push, 4), (pop), (push, 7)]")
+	print("Stack1:", stack1)
+	stack2.push(1)
+	stack2.pop()
+	stack2.pop()
+	stack2.push(9)
+	print("Stack2:[(push, 1), (pop), (pop), (push, 9)]")
+	print("Stack2:", stack2)
+	print("Stack1 size:", stack1.size())
+	print("Stack2 size:", stack2.size())
+	print("Stack1 is empty?", stack1.isEmpty())
+	print("Stack2 is empty?", stack2.isEmpty())
