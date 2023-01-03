469a35e7a8ee9e87a31137f96fbcf00c26679f9d
EduardoF10 <eduardo.fuertes1@upr.edu>
2023-01-03 18:41:23 -0400

    Finished implementing NQueens class

--- problem_solving/n_queens.py
+++ problem_solving/n_queens.py
@@ -4,112 +4,74 @@ This module defines the n-queens class.
 """
 
 
-def viewStates(stateList):
-	if stateList == None or len(stateList) == 0:
-		return None
-	if len(stateList) == 1:
-		return "[" + str(stateList[0].state) + "]"
-	result = "["
-	for i in range(len(stateList) - 1):
-		result += str(stateList[i].state) + ", "
-	result += str(stateList[-1].state) + "]"
-	return result
 
+import sys 
+import os
+sys.path.append(os.path.abspath("../data_structures"))
+from stack import Stack
 
-class State:
+class NQueens:
 
-	def __init__(self, state = None):
-		self.state = state
+	def __init__(self, n):
+		self.initialState = self.State([None] * n)
 
-	def getState(self):
-		return self.state
+	def expand(self, state):
 
-	def nextStates(self):
-		if self.state == None:
+		chessCols = state.chessCols
+
+		if state == None:
 			return None
 
-		possibleRows = set(range(len(self.state)))
+		possibleRows = set(range(len(chessCols)))
 
 		# Getting first empty column index
-		for i in range(len(self.state)):
-			if self.state[i] == None:
+		for i in range(len(chessCols)):
+			if chessCols[i] == None:
 				colIndex = i
 				break
-
-
+				
 		# Discarding invalid rows
 		for col in range(colIndex):
 			# Horizontally
-			possibleRows.discard(self.state[col])
+			possibleRows.discard(chessCols[col])
 			# Diagonally
-			possibleRows.discard(self.state[col] + (colIndex - col))
-			possibleRows.discard(self.state[col] - (colIndex - col))
+			possibleRows.discard(chessCols[col] + (colIndex - col))
+			possibleRows.discard(chessCols[col] - (colIndex - col))
 
 		if len(possibleRows) == 0:
 			return None
 		else:
 			states = []
 			for row in possibleRows:
-				newState = list(self.state)
-				newState[colIndex] = row
-				states.append(State(newState))
+				newCols = list(chessCols)
+				newCols[colIndex] = row
+				states.append(self.State(newCols))
 		return states
 
-
-import sys 
-import os
-sys.path.append(os.path.abspath(".."))
-from data_structures import stack
-
-class NQueens:
-
-	def __init__(self, n):
-		self.initialState = State([None] * n)
-
-	def isGoal(self, stateObj):
-		return stateObj.state[n - 1] != None
+	def viewStates(stateList):
+		if stateList == None or len(stateList) == 0:
+			return None
+		if len(stateList) == 1:
+			return "[" + str(stateList[0].chessCols) + "]"
+		result = "["
+		for i in range(len(stateList) - 1):
+			result += str(stateList[i].chessCols) + ", "
+		result += str(stateList[-1].chessCols) + "]"
+		return result
 
 
-	
+	class State:
 
+		def __init__(self, cols = None):
+			self.chessCols = cols
 
-
-class Main():
-
-	def __init__(self, problem):
-		self.problem = problem
-		self.frontier = stack.Stack([problem.initialState])
-
-	def search(self):
-		while True:
-			# Frontier is empty; no solution
-			if self.frontier.isEmpty():
-				return None
-
-			# Pop state from frontier
-			state = self.frontier.pop()
-
-			# State contains goal; solution
-			if self.problem.isGoal(state):
-				return state
-
-			# Expand next states
-			nextStates = state.nextStates()
-			if nextStates != None:
-				self.frontier.pushItems(nextStates)
+		def isGoal(self):
+			if self.chessCols == None:
+				return False
+			return self.chessCols[len(self.chessCols) - 1] != None
 
 
 # Example usage
 if __name__ == '__main__':
-
-	for n in range(1, 9):
-		print("N =", n)
-		problem = NQueens(n)
-		searchAlgorithm = Main(problem)
-
-		solution = searchAlgorithm.search()
-		if solution == None:
-			print("No solution")
-		else:
-			print(solution.state)
+	pass
 			
